eeddbae2025e3ee7918cbc50392eadfbce73be65
JhaceF <jhacef@proton.me>
2025-10-22 16:13:37 -0500

    CPUPlayer Class

---
+++ NimGame/CPUPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimGame
+{
+    public class CPUPlayer
+    {
+
+        public static void testMeth()
+        {
+
+        }
+    }
+}
